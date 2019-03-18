85752f97f441c681cdf18f3bbe175038fb21c41a
jisongzhou <568777035@qq.com>
2019-03-18 16:08:41 +0800

    修正包名

--- src/main/java/com/yfny/servicetips/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yfny/servicetips/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.yfny.servicetips.exception;
 
-import com.yfny.servicecommon.util.InvokeResult;
+import com.yfny.utilscommon.util.InvokeResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
